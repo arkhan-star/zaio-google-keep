bb0281743f34a874f0abb61bc333ee583f801464
Al-Ridhaa Khan <alridhaakhan@gmail.com>
2026-03-03 09:27:33 +0200

    Initial commit for the project

---
+++ index.js
@@ -0,0 +1,5 @@
+//Two pieces of input are used to create a component in the page when a button is clicked. The first input is used as the text in the component and the second input is used as the color of the text. The component is added to the page when the button is clicked.
+// Input Process Output Pattern
+
+function readInput() {
+    let title = document.getElementById("title").value;
